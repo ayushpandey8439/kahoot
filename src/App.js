be6e7e470176a23bdd42f63afe0435b71725b59c
Ayush Pandey <ayushpandey8439@gmail.com>
2020-11-03 17:22:49 +0100

    Homepage routing setup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/Home/Home";
 function App() {
   return (
     <div className="App">
+      <Router></Router>
       <Home></Home>
     </div>
   );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,29 @@
 import React from "react";
-import { Form } from "react-bootstrap";
+import { Form, Button, InputGroup } from "react-bootstrap";
 import "./Home.scss";
 import Loader from "./../Loader/Loader";
+
 function Home() {
+  function enterQuiz() {
+    console.log("Entering the quiz now");
+  }
   return (
     <div className="home_container">
       <h1 className="title">Kahoot</h1>
-      <Form>
-        <Form.Control type="text" size="lg" placeholder="QuizID"></Form.Control>
+      <Form onSubmit={enterQuiz()}>
+        <InputGroup>
+          <Form.Control
+            type="text"
+            size="lg"
+            placeholder="QuizID"
+            maxLength="5"
+          ></Form.Control>
+          <InputGroup.Append>
+            <Button variant="secondary" type="submit">
+              Enter
+            </Button>
+          </InputGroup.Append>
+        </InputGroup>
       </Form>
       <Loader></Loader>
     </div>
